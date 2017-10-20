45b23dfbe578a900d08fc4a5e20f4c9b209e8e4f
Michael Sevestre <michael@design2code.ca>
2017-10-20 14:49:16 -0400

    Fixes #188 draft watermark support

--- src/MoBi.UI/Views/MoBiMainView.cs
+++ src/MoBi.UI/Views/MoBiMainView.cs
@@ -49,7 +49,6 @@ namespace MoBi.UI.Views
          WindowState = FormWindowState.Maximized;
          base.OnLoad(e);
          _presenter.RestoreLayout();
-         _presenter.Run();
          ResumeLayout();
          _presenter.Run();
          Refresh();
