ea86dc40d90268bce771a30f2d7e27dd2836970c
Michael Sevestre <michael@design2code.ca>
2017-11-09 11:32:30 -0500

    fixes #197 copyright (#198)

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Open Systems Pharmacology")]
 [assembly: AssemblyProduct("MoBi")]
-[assembly: AssemblyCopyright("Copyright © 2017 - Open Systems Pharmacology Community")]
+[assembly: AssemblyCopyright("Copyright © 2002-2017 - Open Systems Pharmacology Community")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("7.2.0.0")]
 [assembly: AssemblyFileVersion("7.2.0.0")]
