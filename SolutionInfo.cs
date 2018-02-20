b9bf5afacf8bac508ee457a66fa9d0a700c67453
Michael Sevestre <michael@design2code.ca>
2018-02-20 09:16:34 -0500

    Update to OSPSuite.Core 7.2.2

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Open Systems Pharmacology")]
 [assembly: AssemblyProduct("MoBi")]
-[assembly: AssemblyCopyright("Copyright © 2002-2017 - Open Systems Pharmacology Community")]
+[assembly: AssemblyCopyright("Copyright © 2002-2018 - Open Systems Pharmacology Community")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("7.2.0.0")]
-[assembly: AssemblyFileVersion("7.2.0.0")]
+[assembly: AssemblyVersion("7.2.2.0")]
+[assembly: AssemblyFileVersion("7.2.2.0")]
 [assembly: AssemblyInformationalVersion("")]
 
 [assembly: InternalsVisibleTo("MoBi.Tests")]
