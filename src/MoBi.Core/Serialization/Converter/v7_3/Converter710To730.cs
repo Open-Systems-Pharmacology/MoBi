2e203b1a7d04e32e42b3addd219ba1ba6e3bb5db
Michael Sevestre <michael@design2code.ca>
2018-05-16 12:59:59 -0400

    Fixes #270 Project Conversion MoBi 7.3 : IsDefault true for all parameters (#273)

---
+++ src/MoBi.Core/Serialization/Converter/v7_3/Converter710To730.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Linq;
+using MoBi.Core.Domain.Model;
+
+namespace MoBi.Core.Serialization.Converter.v7_3
+{
+   public class Converter710To730 : IMoBiObjectConverter
+   {
+      private readonly OSPSuite.Core.Converter.v7_3.Converter710To730 _coreConverter;
+
+      public Converter710To730(OSPSuite.Core.Converter.v7_3.Converter710To730 coreConverter)
+      {
+         _coreConverter = coreConverter;
+      }
+
+      public bool IsSatisfiedBy(int version)
+      {
+         return version == ProjectVersions.V7_1_0;
+      }
+
+      public (int convertedToVersion, bool conversionHappened) Convert(object objectToUpdate, IMoBiProject project)
+      {
+         return _coreConverter.Convert(objectToUpdate);
+      }
+
+      public (int convertedToVersion, bool conversionHappened) ConvertXml(XElement element, IMoBiProject project)
+      {
+         return _coreConverter.ConvertXml(element);
+      }
+   }
+}
