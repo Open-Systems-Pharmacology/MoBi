5fcccffbc9239e1121ef0631389c153a107ecef2
Michael Sevestre <michael@design2code.ca>
2019-04-16 16:31:44 -0400

    Fixes wrong event

--- src/MoBi.Presentation/DTO/SumFormulaDTO.cs
+++ src/MoBi.Presentation/DTO/SumFormulaDTO.cs
@@ -4,18 +4,9 @@ namespace MoBi.Presentation.DTO
 {
    public class SumFormulaDTO : FormulaBuilderDTO
    {
-      private string _variablePattern;
       public string Variable { get; set; }
       public IEnumerable<IDescriptorConditionDTO> VariableCriteria { get; set; }
 
-      public string VariablePattern
-      {
-         get => _variablePattern;
-         set
-         {
-            _variablePattern = value;
-            OnPropertyChanged(() => FormulaString);
-         }
-      }
+      public string VariablePattern { get; set; }
    }
 }
