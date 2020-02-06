cbac2ca9f42e63810aae5c2b9a16b1507aa5a2b0
Michael Sevestre <michael@design2code.ca>
2020-02-06 17:34:37 -0500

    Fixes #422 export cpp

---
+++ src/MoBi.Presentation/UICommand/ExportODEForMatlabUICommand.cs
@@ -0,0 +1,21 @@
+﻿using MoBi.Core.Domain.Model;
+using MoBi.Presentation.Tasks.Edit;
+using OSPSuite.Presentation.UICommands;
+
+namespace MoBi.Presentation.UICommand
+{
+   internal class ExportODEForMatlabUICommand : ObjectUICommand<IMoBiSimulation>
+   {
+      private readonly IEditTasksForSimulation _editTasksForSimulation;
+
+      public ExportODEForMatlabUICommand(IEditTasksForSimulation editTasksForSimulation)
+      {
+         _editTasksForSimulation = editTasksForSimulation;
+      }
+
+      protected override void PerformExecute()
+      {
+         _editTasksForSimulation.ExportODEForMatlab(Subject);
+      }
+   }
+}
