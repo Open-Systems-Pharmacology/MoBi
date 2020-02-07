19098fc44079382ccfd913453f90601dc58fd773
Michael Sevestre <michael@design2code.ca>
2020-02-06 19:34:19 -0500

    442 export cpp (#444)
    * Fixes #422 export cpp
    
    * Fixes #422 export cpp
    
    * Fixes #422 export cpp
    
    * Also generates new templates

---
+++ src/MoBi.Presentation/UICommand/ExportODEForRUICommand.cs
@@ -0,0 +1,21 @@
+﻿using MoBi.Core.Domain.Model;
+using MoBi.Presentation.Tasks.Edit;
+using OSPSuite.Presentation.UICommands;
+
+namespace MoBi.Presentation.UICommand
+{
+  internal class ExportODEForRUICommand : ObjectUICommand<IMoBiSimulation>
+   {
+      private readonly IEditTasksForSimulation _editTasksForSimulation;
+
+      public ExportODEForRUICommand(IEditTasksForSimulation editTasksForSimulation)
+      {
+         _editTasksForSimulation = editTasksForSimulation;
+      }
+
+      protected override void PerformExecute()
+      {
+         _editTasksForSimulation.ExportODEForR(Subject);
+      }
+   }
+}
