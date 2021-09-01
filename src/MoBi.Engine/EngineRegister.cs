fdcb36af0b3ef0dbcae92dbe9d042add1e5964d7
Abdel Rodríguez <abdelr1982@gmail.com>
2021-09-01 15:49:21 +0200

    breaking commit

--- src/MoBi.Engine/EngineRegister.cs
+++ src/MoBi.Engine/EngineRegister.cs
@@ -14,31 +14,12 @@ namespace MoBi.Engine
             x.AssemblyContainingType<EngineRegister>();
             x.WithConvention(new OSPSuiteRegistrationConvention(registerConcreteType: true));
             x.ExcludeType<UnitDefinitionImporter>();
-            x.ExcludeType<CompartmentImporter>();
-            x.ExcludeType<SpeciesImporter>();
-            x.ExcludeType<ParameterImporter>();
             x.ExcludeType<FunctionDefinitionImporter>();
-            x.ExcludeType<ReactionImporter>();
-            x.ExcludeType<AssignmentImporter>();
-            x.ExcludeType<EventImporter>();
-            x.ExcludeType<InitialAssignmentImporter>();
-            x.ExcludeType<RuleImporter>();
-            x.ExcludeType<SBMLImporterRepository>();
             x.ExcludeType<ASTHandler>();
          });
 
-         container.Register<SBMLImporterRepository, SBMLImporterRepository>();
-         container.Register<CompartmentImporter, CompartmentImporter>();
-         container.Register<SpeciesImporter, SpeciesImporter>();
          container.Register<IUnitDefinitionImporter, UnitDefinitionImporter>(LifeStyle.Singleton);
          container.Register<IFunctionDefinitionImporter, FunctionDefinitionImporter>(LifeStyle.Singleton);
-         container.Register<EventImporter, EventImporter>();
-         container.Register<AssignmentImporter, AssignmentImporter>();
-         container.Register<InitialAssignmentImporter, InitialAssignmentImporter>();
-         container.Register<RuleImporter, RuleImporter>();
-         container.Register<ParameterImporter, ParameterImporter>();
-         container.Register<ReactionImporter, ReactionImporter>();
-         container.Register<ASTHandler, ASTHandler>();
          container.Register<ISbmlTask, SbmlTask>();
       }
    }
