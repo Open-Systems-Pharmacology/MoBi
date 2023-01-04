191e732d34d090734eea9d5811fedcbe9942a40b
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2023-01-04 11:54:51 -0500

    Fixes #645 Individual Management

---
+++ src/MoBi.Presentation/MenusAndBars/ContextMenus/ContextMenuForIndividualBuildingBlock.cs
@@ -0,0 +1,13 @@
+﻿using MoBi.Core.Domain.Model;
+using OSPSuite.Core.Domain.Builder;
+using OSPSuite.Core.Domain.Services;
+
+namespace MoBi.Presentation.MenusAndBars.ContextMenus
+{
+   public class ContextMenuForIndividualBuildingBlock : ContextMenuForBuildingBlock<IndividualBuildingBlock>
+   {
+      public ContextMenuForIndividualBuildingBlock(IMoBiContext context, IObjectTypeResolver objectTypeResolver) : base(context, objectTypeResolver)
+      {
+      }
+   }
+}
