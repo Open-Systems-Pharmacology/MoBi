baf208bda759c4f660d4b996b176be7a68b7b3c9
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2023-09-13 13:40:31 -0400

    Fixes #1087 Error when trying to load a Simulation (#1108)
    * Fixes #1087 Error when trying to load a Simulation
    
    * PR feedback

--- src/MoBi.Core/Services/SimulationLoader.cs
+++ src/MoBi.Core/Services/SimulationLoader.cs
@@ -54,8 +54,13 @@ namespace MoBi.Core.Services
       {
          var moBiSimulation = simulation;
 
-         // simulation.MoBiBuildConfiguration.AllBuildingBlockInfos().Each(checkTemplateBuildingBlock);
          var project = _context.CurrentProject;
+
+         renameCollidingEntities(simulation.Modules, project.Modules);
+         renameCollidingEntities(simulation.Configuration.ExpressionProfiles, project.ExpressionProfileCollection);
+         
+         if(simulation.Configuration.Individual != null)
+            renameCollidingEntities(new[] { simulation.Configuration.Individual }, project.IndividualsCollection);
 
          if (shouldCloneSimulation)
             moBiSimulation = cloneSimulation(moBiSimulation);
@@ -68,6 +73,12 @@ namespace MoBi.Core.Services
 
          moBiSimulation.HasChanged = true;
          loadCommand.AddCommand(new AddSimulationCommand(moBiSimulation));
+      }
+
+      private void renameCollidingEntities(IEnumerable<IObjectBase> entitiesToRename, IReadOnlyList<IWithName> existingEntities)
+      {
+         var takenNames = existingEntities.AllNames();
+         entitiesToRename.Where(x => takenNames.Contains(x.Name)).Each(x => _nameCorrector.AutoCorrectName(takenNames, x));
       }
 
       public ICommand AddSimulationToProject(SimulationTransfer simulationTransfer)
@@ -102,7 +113,7 @@ namespace MoBi.Core.Services
       {
          var cloneForProjectEntities = _cloneManager.CloneSimulationConfiguration(simulation.Configuration);
          cloneForProjectEntities.ModuleConfigurations.Each(moduleConfiguration => commandCollector.AddCommand(new AddModuleCommand(moduleConfiguration.Module)));
-         
+
          addToProject(commandCollector, cloneForProjectEntities.Individual, individual => new AddIndividualBuildingBlockToProjectCommand(individual));
          cloneForProjectEntities.ExpressionProfiles.Each(expressionProfile => addToProject(commandCollector, expressionProfile, ep => new AddExpressionProfileBuildingBlockToProjectCommand(ep)));
       }
