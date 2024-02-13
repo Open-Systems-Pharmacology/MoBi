9ad818dba007008366d5bbb1203a979270c454ce
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2024-02-13 12:50:12 -0500

    code cleanup

--- src/MoBi.Presentation/Presenter/EditFavoritesPresenterInSimulation.cs
+++ src/MoBi.Presentation/Presenter/EditFavoritesPresenterInSimulation.cs
@@ -9,51 +9,51 @@ using OSPSuite.Core.Services;
 
 namespace MoBi.Presentation.Presenter
 {
-    public interface IEditFavoritesInSimulationPresenter : IEditFavoritesPresenter<IMoBiSimulation>, IEditInSimulationPresenter
-    {
-        IEnumerable<IParameter> Favorites();
-    }
+   public interface IEditFavoritesInSimulationPresenter : IEditFavoritesPresenter<IMoBiSimulation>, IEditInSimulationPresenter
+   {
+      IEnumerable<IParameter> Favorites();
+   }
 
-    public class EditFavoritesInSimulationPresenter : EditFavoritesPresenter<IMoBiSimulation>, IEditFavoritesInSimulationPresenter
-    {
-        public EditFavoritesInSimulationPresenter(
-            IEditFavoritesView view,
-            IFavoriteRepository favoriteRepository,
-            IEntityPathResolver entityPathResolver,
-            IEditParameterListPresenter editParameterListPresenter,
-            IFavoriteTask favoriteTask)
-            : base(view, favoriteRepository, entityPathResolver, editParameterListPresenter, favoriteTask)
-        {
-            ShouldHandleRemovedEvent = x => false; //Can not remove in Simulation
-        }
+   public class EditFavoritesInSimulationPresenter : EditFavoritesPresenter<IMoBiSimulation>, IEditFavoritesInSimulationPresenter
+   {
+      public EditFavoritesInSimulationPresenter(
+         IEditFavoritesView view,
+         IFavoriteRepository favoriteRepository,
+         IEntityPathResolver entityPathResolver,
+         IEditParameterListPresenter editParameterListPresenter,
+         IFavoriteTask favoriteTask)
+         : base(view, favoriteRepository, entityPathResolver, editParameterListPresenter, favoriteTask)
+      {
+         ShouldHandleRemovedEvent = x => false; //Can not remove in Simulation
+      }
 
-        protected override void CacheParameters(IMoBiSimulation projectItem)
-        {
-            var allParameters = projectItem.Model.Root.GetAllChildren<IParameter>();
-            _parameterCache.AddRange(allParameters);
-        }
+      protected override void CacheParameters(IMoBiSimulation projectItem)
+      {
+         var allParameters = projectItem.Model.Root.GetAllChildren<IParameter>();
+         _parameterCache.AddRange(allParameters);
+      }
 
-        protected override bool IsAddedToParent(IObjectBase parent)
-        {
-            var parentContainer = parent as IContainer;
-            return parentContainer != null && Simulation.Model.Root.GetAllContainersAndSelf<IContainer>().Contains(parentContainer);
-        }
+      protected override bool IsAddedToParent(IObjectBase parent)
+      {
+         var parentContainer = parent as IContainer;
+         return parentContainer != null && Simulation.Model.Root.GetAllContainersAndSelf<IContainer>().Contains(parentContainer);
+      }
 
-        public IMoBiSimulation Simulation
-        {
-            get => _projectItem;
-            set => _projectItem = value;
-        }
+      public IMoBiSimulation Simulation
+      {
+         get => _projectItem;
+         set => _projectItem = value;
+      }
 
-        public IEnumerable<IParameter> Favorites()
-        {
-            return _editParameterListPresenter.EditedParameters;
-        }
+      public IEnumerable<IParameter> Favorites()
+      {
+         return _editParameterListPresenter.EditedParameters;
+      }
 
-        public override void Edit(IMoBiSimulation projectItem)
-        {
-            base.Edit(projectItem);
-            _editParameterListPresenter.SetVisibility(PathElementId.Simulation, isVisible:false );
-        }
-    }
+      public override void Edit(IMoBiSimulation projectItem)
+      {
+         base.Edit(projectItem);
+         _editParameterListPresenter.SetVisibility(PathElementId.Simulation, isVisible: false);
+      }
+   }
 }
