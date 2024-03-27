1b0bd249b7707ccacb2a90eaa2b13e3e1c620636
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2024-03-27 08:06:02 -0400

    Fixes #1289 Organ export dialog - typo

--- src/MoBi.Assets/AppConstants.cs
+++ src/MoBi.Assets/AppConstants.cs
@@ -1590,7 +1590,7 @@ namespace MoBi.Assets
          public static readonly string ChangeDefaultCurveForNewSimulations = "Change default curve for new simulations";
          public static string SelectEntitiesThatWillBeReplaced(string entityType) => $"Select {entityType.Pluralize()} that will be replaced";
          public static string SelectEntitiesThatWillBeReplacedDescription(string entityType, string buildingBlockName) => $"<b>Selected <i>{entityType.Pluralize()}</i> will replace existing <i>{entityType.Pluralize()}</i> in the building block <i>{buildingBlockName}</i></b>";
-         public static string ExportContainerDescription(string exportedContainerPath) => $"<b>Select a <i>file path</i> and optional <i>individual</i> and <i>expression profiles</i> for container export. Parameters from the <i>individual</i> and <i>expression profiles</i> that match the path {exportedContainerPath} will be addd to the container before exporting.</b>";
+         public static string ExportContainerDescription(string exportedContainerPath) => $"<b>Select a <i>file path</i> and optional <i>individual</i> and <i>expression profiles</i> for container export. Parameters from the <i>individual</i> and <i>expression profiles</i> that match the path {exportedContainerPath} will be added to the container before exporting.</b>";
          public static readonly string SelectIndividualAndPathForContainerExport = "Select an individual and path for container export";
          public static readonly string AddInterval = "Add Interval";
          public static string CouldNotResolveSource(string sourceType) => $"{sourceType} source not defined";
