9309f03b5921a31eb9b6882fa03b4637bc3ca246
Peter de Witte <peter@whitewebinnovations.com>
2024-04-25 12:10:38 +0200

    indentation fixed

--- src/MoBi.Presentation/MenusAndBars/ContextMenus/MultipleParameterIdentificationContextMenuFactory.cs
+++ src/MoBi.Presentation/MenusAndBars/ContextMenus/MultipleParameterIdentificationContextMenuFactory.cs
@@ -16,44 +16,44 @@ using ParameterIdentification = OSPSuite.Core.Domain.ParameterIdentifications.Pa
 
 namespace MoBi.Presentation.MenusAndBars.ContextMenus
 {
-    public class MultipleParameterIdentificationContextMenuFactory : MultipleNodeContextMenuFactory<ClassifiableParameterIdentification>
-    {
-        private readonly IMoBiContext _context;
-        private readonly IContainer _container;
+   public class MultipleParameterIdentificationContextMenuFactory : MultipleNodeContextMenuFactory<ClassifiableParameterIdentification>
+   {
+      private readonly IMoBiContext _context;
+      private readonly IContainer _container;
 
-        public MultipleParameterIdentificationContextMenuFactory(IMoBiContext context, IContainer container)
-        {
-            _context = context;
-            _container = container;
-        }
+      public MultipleParameterIdentificationContextMenuFactory(IMoBiContext context, IContainer container)
+      {
+         _context = context;
+         _container = container;
+      }
 
-        public override bool IsSatisfiedBy(IReadOnlyList<ITreeNode> treeNodes, IPresenterWithContextMenu<IReadOnlyList<ITreeNode>> presenter)
-        {
-            return treeNodes.All(node => node.IsAnImplementationOf<ParameterIdentificationNode>());
-        }
+      public override bool IsSatisfiedBy(IReadOnlyList<ITreeNode> treeNodes, IPresenterWithContextMenu<IReadOnlyList<ITreeNode>> presenter)
+      {
+         return treeNodes.All(node => node.IsAnImplementationOf<ParameterIdentificationNode>());
+      }
 
-        protected override IContextMenu CreateFor(IReadOnlyList<ClassifiableParameterIdentification> parameterIdentifications, IPresenterWithContextMenu<IReadOnlyList<ITreeNode>> presenter)
-        {
-            var paramIdentificationList = parameterIdentifications.Select(pi => pi.ParameterIdentification).ToList().AsReadOnly();
-            return new MultipleParameterIdentificationContextMenu(paramIdentificationList, _context, _container);
-        }
-    }
+      protected override IContextMenu CreateFor(IReadOnlyList<ClassifiableParameterIdentification> parameterIdentifications, IPresenterWithContextMenu<IReadOnlyList<ITreeNode>> presenter)
+      {
+         var paramIdentificationList = parameterIdentifications.Select(pi => pi.ParameterIdentification).ToList().AsReadOnly();
+         return new MultipleParameterIdentificationContextMenu(paramIdentificationList, _context, _container);
+      }
+   }
 
-    public class MultipleParameterIdentificationContextMenu : ContextMenu<IReadOnlyList<ParameterIdentification>, IMoBiContext>
-    {
-        public MultipleParameterIdentificationContextMenu(IReadOnlyList<ParameterIdentification> parameterIdentifications, IMoBiContext context, IContainer container)
-            : base(parameterIdentifications, context, container)
-        {
-        }
+   public class MultipleParameterIdentificationContextMenu : ContextMenu<IReadOnlyList<ParameterIdentification>, IMoBiContext>
+   {
+      public MultipleParameterIdentificationContextMenu(IReadOnlyList<ParameterIdentification> parameterIdentifications, IMoBiContext context, IContainer container)
+         : base(parameterIdentifications, context, container)
+      {
+      }
 
-        protected override IEnumerable<IMenuBarItem> AllMenuItemsFor(IReadOnlyList<ParameterIdentification> parameterIdentifications, IMoBiContext context)
-        {
-            yield return ObjectBaseCommonContextMenuItems.AddToJournal(parameterIdentifications, _container);
+      protected override IEnumerable<IMenuBarItem> AllMenuItemsFor(IReadOnlyList<ParameterIdentification> parameterIdentifications, IMoBiContext context)
+      {
+         yield return ObjectBaseCommonContextMenuItems.AddToJournal(parameterIdentifications, _container);
 
-            yield return CreateMenuButton.WithCaption(AppConstants.MenuNames.Delete)
-                .WithCommandFor<RemoveMultipleParameterIdentificationsUICommand, IReadOnlyList<ParameterIdentification>>(parameterIdentifications, _container)
-                .WithIcon(ApplicationIcons.Delete)
-                .AsGroupStarter();
-        }
-    }
+         yield return CreateMenuButton.WithCaption(AppConstants.MenuNames.Delete)
+            .WithCommandFor<RemoveMultipleParameterIdentificationsUICommand, IReadOnlyList<ParameterIdentification>>(parameterIdentifications, _container)
+            .WithIcon(ApplicationIcons.Delete)
+            .AsGroupStarter();
+      }
+   }
 }
