0e864ef50b04ad6a948c986cef0cd472b9b189b3
Peter de Witte <peter@whitewebinnovations.com>
2024-06-10 09:12:54 +0200

    Started adding ContextMenuForSimulationEntities

---
+++ src/MoBi.Presentation/MenusAndBars/ContextMenus/ContextMenuFactoryForSimulationEntities.cs
@@ -0,0 +1,34 @@
+﻿using MoBi.Core.Domain.Model;
+using MoBi.Presentation.DTO;
+using MoBi.Presentation.Presenter;
+using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Builder;
+using OSPSuite.Presentation.Core;
+using OSPSuite.Presentation.Presenters;
+using OSPSuite.Presentation.Presenters.ContextMenus;
+using OSPSuite.Utility.Extensions;
+
+namespace MoBi.Presentation.MenusAndBars.ContextMenus
+{
+   public class ContextMenuFactoryForSimulationEntities : ContextMenuForObjectBaseDTOSpecificationFactory<IContainer>
+   {
+      public ContextMenuFactoryForSimulationEntities(IMoBiContext context) : base(context)
+      {
+      }
+
+      public override IContextMenu CreateFor(ObjectBaseDTO objectBaseDTO, IPresenterWithContextMenu<IViewItem> presenter)
+      {
+         var contextMenu = _context.Resolve<ContextMenuForContainer>();
+         return contextMenu.InitializeWith(objectBaseDTO, presenter);
+      }
+
+      protected override bool IsSatisfiedBy(IEntity entity, IPresenterWithContextMenu<IViewItem> presenter)
+      {
+         return presenter.IsAnImplementationOf<HierarchicalSimulationPresenter>()
+                && entity.IsAnImplementationOf<IEntity>()
+                && entity.IsAnImplementationOf<HierarchicalSimulationPresenter>();
+         ////neighborhoods will be dealt with separately
+         //&& !entity.IsNamed(Constants.NEIGHBORHOODS);
+      }
+   }
+}
