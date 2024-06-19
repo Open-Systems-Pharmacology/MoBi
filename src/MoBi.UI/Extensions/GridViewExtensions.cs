a6df50b075a15fa5a0eded3b1ffa386d51275dde
Peter de Witte <peter@whitewebinnovations.com>
2024-06-19 20:02:47 +0200

    Fixes #159: The contents of the cell are now shown on the first row in the tooltip.  (#1357)
    * Added cellvalue of cell under mousecursor to the top of the tooltip.
    
    * Added comment and newline for clarity.
    
    * Small code improvements
    
    * Minor code style imporovements
    
    * Improved look and feel of tooltip
    
    * PR feedback and using the tooltip in another view
    
    ---------
    
    Co-authored-by: Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>

--- src/MoBi.UI/Extensions/GridViewExtensions.cs
+++ src/MoBi.UI/Extensions/GridViewExtensions.cs
@@ -1,5 +1,8 @@
-﻿using DevExpress.Utils;
+﻿using System;
+using System.Drawing;
+using DevExpress.Utils;
 using DevExpress.XtraGrid.Views.Grid;
+using OSPSuite.Core.Domain;
 using OSPSuite.UI.Controls;
 
 namespace MoBi.UI.Extensions
@@ -20,6 +23,30 @@ namespace MoBi.UI.Extensions
       {
          gridView.OptionsCustomization.AllowGroup = false;
          gridView.OptionsView.ShowGroupPanel = false;
+      }
+
+      /// <summary>
+      /// Creates a ToolTipControlInfo for the <paramref name="toolTipSubject"/> using the <paramref name="toolTipGenerator"/> function.
+      /// If a column can be determined from <paramref name="point"/>, then the column caption and cell display text are used with <paramref name="toolTipGenerator"/>.
+      /// </summary>
+      public static ToolTipControlInfo CreateToolTipControlInfoFor<T>(this GridView gridView, T toolTipSubject, Point point, Func<T, string, string, SuperToolTip> toolTipGenerator) where T : IWithName
+      {
+         var columnName = string.Empty;
+         var cellValue = string.Empty;
+
+         var gridHitInfo = gridView.CalcHitInfo(point);
+         if (gridHitInfo.Column != null)
+         {
+            columnName = gridHitInfo.Column.Caption;
+            cellValue = gridView.GetRowCellDisplayText(gridHitInfo.RowHandle, gridHitInfo.Column);
+         }
+
+         // Adds a super-tooltip for the current active object. Uniquely identified by the combination of toolTipSubject.Name and columnName.
+         return new ToolTipControlInfo(toolTipSubject.Name + columnName, string.Empty)
+         {
+            SuperTip = toolTipGenerator(toolTipSubject, columnName, cellValue), 
+            ToolTipType = ToolTipType.SuperTip
+         };
       }
    }
 }
