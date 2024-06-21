11a00851027909c6c9ec72521ef05affb77afb46
benjaperez1983 <benjamin.perez@gmail.com>
2024-06-21 12:12:11 +0200

    #1358 Add Tests for MoveBuildingBlockToComuleCommand

---
+++ tests/MoBi.Tests/Core/Commands/MoveBuildingBlockToModuleCommandSpecs.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using FakeItEasy;
+using MoBi.Core.Domain.Model;
+using MoBi.Core.Domain.Services;
+using MoBi.Core.Exceptions;
+using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Builder;
+
+namespace MoBi.Core.Commands
+{
+   public class concern_for_MoveBuildingBlockToModuleCommand : ContextSpecification<MoveBuildingBlockToModuleCommand>
+   {
+      protected Module _targetModule;
+      protected Module _sourceModule;
+      protected IMoBiContext _context;
+      protected MoBiProject _project;
+      protected RegisterTask _registrationTask;
+      protected WithIdRepository _withIdRepository;
+      protected ReactionBuildingBlock _newReactionBuildingBlock;
+
+      protected override void Context()
+      {
+         _newReactionBuildingBlock = new ReactionBuildingBlock().WithId("reactionId");
+         _sourceModule= new Module().WithId("sourceModuleId");
+         _sourceModule.Add(_newReactionBuildingBlock);
+         _targetModule = new Module().WithId("targetModuleId");
+         
+         _withIdRepository = new WithIdRepository();
+         _registrationTask = new RegisterTask(_withIdRepository);
+         sut = new MoveBuildingBlockToModuleCommand(_newReactionBuildingBlock, _targetModule);
+         _project = new MoBiProject();
+         _context = A.Fake<IMoBiContext>();
+         A.CallTo(() => _context.CurrentProject).Returns(_project);
+
+         _targetModule.Add(new SpatialStructure().WithId("SpatialStructure"));
+         _targetModule.Add(new MoleculeBuildingBlock().WithId("Molecule"));
+      }
+   }
+
+   public class When_executing_the_move_blocks_to_module_command : concern_for_MoveBuildingBlockToModuleCommand
+   {
+      protected override void Because()
+      {
+         sut.Execute(_context);
+      }
+
+      [Observation]
+      public void the_building_blocks_must_be_moved_to_the_module()
+      {
+         _targetModule.Reactions.ShouldBeEqualTo(_newReactionBuildingBlock);
+         _sourceModule.Reactions.ShouldBeNull();
+      }
+   }
+
+   public class When_executing_the_move_blocks_to_module_command_with_No_Module : concern_for_MoveBuildingBlockToModuleCommand
+   {
+      protected override void Context()
+      {
+         base.Context();
+         _newReactionBuildingBlock.Module = null;
+      }
+
+      [Observation]
+      public void should_throw_an_exception()
+      {
+         The.Action(() => sut.Execute(_context)).ShouldThrowAn<NullReferenceException>();
+      }
+   }
+}
