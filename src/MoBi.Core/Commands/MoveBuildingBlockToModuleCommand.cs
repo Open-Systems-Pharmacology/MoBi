b6413c09b0d1986ed08599af705415b1edc5889d
benjaperez1983 <benjamin.perez@gmail.com>
2024-06-24 17:48:24 +0200

    #1358 Cleanup code

--- src/MoBi.Core/Commands/MoveBuildingBlockToModuleCommand.cs
+++ src/MoBi.Core/Commands/MoveBuildingBlockToModuleCommand.cs
@@ -1,11 +1,8 @@
 ﻿using System.Collections.Generic;
 using MoBi.Assets;
-using MoBi.Core.Domain.Model;
-using NPOI.SS.Formula.Functions;
 using OSPSuite.Assets;
 using OSPSuite.Core.Domain;
 using OSPSuite.Core.Domain.Builder;
-using OSPSuite.Utility.Extensions;
 
 namespace MoBi.Core.Commands
 {
@@ -21,22 +18,12 @@ namespace MoBi.Core.Commands
          Description = AppConstants.Commands.MoveBuildingBlockToModule(buildingBlockToMove.DisplayName, targetModule.Name);
 
          var allCommands = new List<IMoBiCommand>
-      {
-         var allCommands = new List<IMoBiCommand>();
-
-         //add the required commands
+         {
             new RemoveBuildingBlockFromModuleCommand<IBuildingBlock>(buildingBlockToMove, buildingBlockToMove.Module),
             new AddBuildingBlockToModuleCommand<IBuildingBlock>(buildingBlockToMove, targetModule)
          };
 
          AddRange(allCommands);
-
-         //now execute all commands
-         base.Execute(context);
-
-         //clear references
-         _destinationModule = null;
-         _buildingBlockToMove = null;
       }
    }
 }
