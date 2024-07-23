fddbb33e65e6deed27c1582c5a507e98022d4781
benjaperez1983 <benjamin.perez@gmail.com>
2024-07-23 16:05:50 +0200

    #1343 tests for renaming

--- tests/MoBi.Tests/Presentation/Tasks/EditTaskForModuleSpecs.cs
+++ tests/MoBi.Tests/Presentation/Tasks/EditTaskForModuleSpecs.cs
@@ -8,6 +8,8 @@ using MoBi.Presentation.Tasks.Interaction;
 using OSPSuite.BDDHelper;
 using OSPSuite.BDDHelper.Extensions;
 using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Builder;
+using Module = OSPSuite.Core.Domain.Module;
 
 namespace MoBi.Presentation.Tasks
 {
@@ -47,6 +49,37 @@ namespace MoBi.Presentation.Tasks
       public void all_modules_in_project_should_be_listed()
       {
          _prohibitedNames.ShouldContain("newModule1", "newModule2");
+      }
+   }
+
+   public class When_renaming_a_module : ContextSpecification<EditTaskForModule>
+   {
+      private Module _module;
+      private List<IObjectBase> _existingObjectsInParent;
+      private EditTaskForModule _sut;
+      private IInteractionTaskContext _interactionTaskContext;
+
+      protected override void Context()
+      {
+         _interactionTaskContext = A.Fake<IInteractionTaskContext>();
+         _sut = A.Fake<EditTaskForModule>(options => options
+            .WithArgumentsForConstructor(() => new EditTaskForModule(_interactionTaskContext))
+            .CallsBaseMethods());
+
+         _module = new Module().WithName("newModule1");
+         _existingObjectsInParent = new List<IObjectBase> { new Module().WithName("newModule2") };
+         A.CallTo(() => _interactionTaskContext.NamingTask.RenameFor(A<IObjectBase>.Ignored, A<IReadOnlyList<string>>.Ignored)).Returns("Module1");
+      }
+
+      protected override void Because()
+      {
+         _sut.Rename(_module, _existingObjectsInParent, new MoleculeBuildingBlock().WithId("newMoleculeBuildingBlockId"));
+      }
+
+      [Observation]
+      public void should_rename_module_but_not_related_objects()
+      {
+         A.CallTo(_sut).Where(x => x.Method.Name.Contains("GetRenameCommandFor") && x.Arguments.Get<IBuildingBlock>(1) == null).MustHaveHappened();
       }
    }
 }
