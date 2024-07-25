86235732ccf77f369fc596d2f92d0700ae186ee0
Benjamin Perez <108261271+benjaperez1983@users.noreply.github.com>
2024-07-25 16:39:03 +0200

    #1364 new passive transport default label when unnamed (#1416)
    * #1364 new passive transport default label when unnamed
    
    * #1364 adapt for any type of container
    
    * #1364 Code Cleanup
    
    * #1364  Changes and tests
    
    * #1364 fix tests
    
    * #1364 tests fix
    
    * #1364 test fix
    
    * #1364 test and code cleanup
    
    * #1364 new window
    
    * #1364 code cleanup

--- src/MoBi.Presentation/Views/IEditParametersInContainerView.cs
+++ src/MoBi.Presentation/Views/IEditParametersInContainerView.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections.Generic;
 using MoBi.Presentation.DTO;
 using MoBi.Presentation.Presenter;
+using OSPSuite.Core.Domain;
 using OSPSuite.Presentation.Views;
 
 namespace MoBi.Presentation.Views
 {
-   public interface IEditParametersInContainerView :  IView<IEditParametersInContainerPresenter>
+   public interface IEditParametersInContainerView : IView<IEditParametersInContainerPresenter>
    {
       void BindTo(IEnumerable<ParameterDTO> dtos);
       EditParameterMode EditMode { get; set; }
       bool ShowBuildMode { get; set; }
-      string ParentName {  set; }
+      string ParentName { set; }
       void SetEditParameterView(IView view);
       void RefreshList();
       void Select(ParameterDTO parameterToSelect);
