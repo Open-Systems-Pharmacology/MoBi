041c4ea9698c61d28f6c25d151bd77749a0d8494
benjaperez1983 <benjamin.perez@gmail.com>
2024-09-18 14:22:33 +0200

    #1527 code review

--- src/MoBi.Assets/AppConstants.cs
+++ src/MoBi.Assets/AppConstants.cs
@@ -1183,7 +1183,7 @@ namespace MoBi.Assets
             return string.Format("Cannot find a parameter for path {1} in simulation {0} during import. Only updates are allowed", name, path);
          }
 
-         public static readonly string CheckDimensionOfAddedParameters = "Check the \"Dimension\" property of the added parameters";
+         public static readonly string CheckDimensionOfAddedParameters = "Check the <b><i>Dimension</i></b> property of the added parameters";
          public static readonly string ThisItNotATemplateBuildingBlock = "This is not the template building block!";
       }
 
