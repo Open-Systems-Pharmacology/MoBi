60bb9d3e0ed6b161ad8015ebcabed8e609c7ff48
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2024-10-24 09:05:45 -0400

    rewording a dialog

--- src/MoBi.Assets/AppConstants.cs
+++ src/MoBi.Assets/AppConstants.cs
@@ -1185,7 +1185,7 @@ namespace MoBi.Assets
             return string.Format("Cannot find a parameter for path {1} in simulation {0} during import. Only updates are allowed", name, path);
          }
 
-         public static readonly string CheckDimensionOfAddedParameters = "Check the <i>Dimension</i> property of the added parameters";
+         public static readonly string CheckDimensionOfAddedParameters = $"The <i>Dimension</i> property of the added parameters will not be automatically set.{Environment.NewLine}Please set the <i>Dimension</i> of all added parameters.";
          public static readonly string ThisItNotATemplateBuildingBlock = "This is not the template building block!";
       }
 
