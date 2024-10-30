b31c69ed73381703ea6130d1598f8899be3963c1
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2024-10-30 09:46:07 -0400

    Fixes #1415 Extend PV and IC BB from PKML

---
+++ src/MoBi.Presentation/UICommand/ExtendInitialConditionsFromInitialConditionsUICommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MoBi.Core.Domain.Model;
+using MoBi.Presentation.Tasks.Interaction;
+using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Builder;
+
+namespace MoBi.Presentation.UICommand
+{
+   public class ExtendInitialConditionsFromInitialConditionsUICommand : ExtendBuildingBlockFromPathAndValuesUICommand<InitialConditionsBuildingBlock, Module, InitialConditionsBuildingBlock, InitialCondition>
+   {
+      public ExtendInitialConditionsFromInitialConditionsUICommand(IInitialConditionsTask<InitialConditionsBuildingBlock> interactionTasksForSource, IMoBiContext context) : base(interactionTasksForSource, interactionTasksForSource, context)
+      {
+      }
+
+      protected override IReadOnlyList<InitialCondition> MapAll(IReadOnlyList<InitialConditionsBuildingBlock> buildingBlocks)
+      {
+         return buildingBlocks.SelectMany(x => x).ToList();
+      }
+   }
+}
