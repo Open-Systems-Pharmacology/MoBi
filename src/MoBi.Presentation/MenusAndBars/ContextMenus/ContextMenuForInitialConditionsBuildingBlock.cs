f102fd225e523c05d41215ae44b2b3d62927314f
Benjamin Perez <108261271+benjaperez1983@users.noreply.github.com>
2024-12-19 12:16:43 +0100

    #1720 Import from excel rename menu (#1728)

--- src/MoBi.Presentation/MenusAndBars/ContextMenus/ContextMenuForInitialConditionsBuildingBlock.cs
+++ src/MoBi.Presentation/MenusAndBars/ContextMenus/ContextMenuForInitialConditionsBuildingBlock.cs
@@ -26,7 +26,7 @@ namespace MoBi.Presentation.MenusAndBars.ContextMenus
          var buildingBlock = _context.Get<InitialConditionsBuildingBlock>(dto.Id);
          base.InitializeWith(dto, presenter);
 
-         _allMenuItems.Add(CreateMenuButton.WithCaption(AppConstants.MenuNames.Import)
+         _allMenuItems.Add(CreateMenuButton.WithCaption(AppConstants.MenuNames.ImportFromExcel)
             .WithIcon(ApplicationIcons.InitialConditionsImport)
             .WithCommandFor<ImportInitialConditionsUICommand, IBuildingBlock>(buildingBlock, _container));
 
