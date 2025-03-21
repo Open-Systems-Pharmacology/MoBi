f9cc87e487e1dba282b475eda6389d7557b4812a
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-03-21 13:30:47 +0100

    Fixes #1794 Can't open existing project, Error: '', hexadecimal value 0x1F, is an invalid character (#1825)

--- src/MoBi.Presentation/Tasks/SerializationTask.cs
+++ src/MoBi.Presentation/Tasks/SerializationTask.cs
@@ -11,6 +11,7 @@ using MoBi.Core.Serialization.Services;
 using MoBi.Core.Serialization.Xml.Services;
 using OSPSuite.Core.Domain;
 using OSPSuite.Core.Domain.Services;
+using OSPSuite.Core.Serialization;
 using OSPSuite.Core.Serialization.Exchange;
 using OSPSuite.Core.Services;
 using OSPSuite.Utility.Extensions;
@@ -110,7 +111,7 @@ namespace MoBi.Presentation.Tasks
       public void SaveModelPart<T>(T entityToSerialize, string filename)
       {
          var xelPart = _xmlSerializationService.SerializeModelPart(entityToSerialize);
-         xelPart.Save(filename);
+         xelPart.PermissiveSave(filename);
       }
 
       public T Load<T>(string fileName, bool resetIds = false)
@@ -120,12 +121,13 @@ namespace MoBi.Presentation.Tasks
 
       public IEnumerable<T> LoadMany<T>(string fileName, bool resetIds = false)
       {
-         _projectConverterLogger.Clear();
-
-         XElement xelRoot = XElement.Load(fileName);
          if (string.IsNullOrEmpty(fileName))
             return Enumerable.Empty<T>();
 
+         _projectConverterLogger.Clear();
+
+         var xelRoot = XElementSerializer.PermissiveLoad(fileName);
+         
          var possibleElementsToDeserialize = retrieveElementsToDeserializeFromFile<T>(xelRoot, fileName).ToList();
 
          var selection = possibleElementsToDeserialize.Count > 1
