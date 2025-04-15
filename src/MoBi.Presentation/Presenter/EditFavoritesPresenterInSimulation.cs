50d659939010a342bb23774e579cf134646edf86
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-04-15 13:24:48 -0400

    Fixes #160 Favorites list: Column "Simulation" is expendable

--- src/MoBi.Presentation/Presenter/EditFavoritesPresenterInSimulation.cs
+++ src/MoBi.Presentation/Presenter/EditFavoritesPresenterInSimulation.cs
@@ -25,6 +25,7 @@ namespace MoBi.Presentation.Presenter
          : base(view, favoriteRepository, entityPathResolver, editParameterListPresenter, favoriteTask)
       {
          ShouldHandleRemovedEvent = x => false; //Can not remove in Simulation
+         UpdateSpecialColumnsVisibility = _editParameterListPresenter.ConfigureForSimulation;
       }
 
       protected override void CacheParameters(IMoBiSimulation projectItem)
@@ -48,12 +49,6 @@ namespace MoBi.Presentation.Presenter
       public IEnumerable<IParameter> Favorites()
       {
          return _editParameterListPresenter.EditedParameters;
-      }
-
-      public override void Edit(IMoBiSimulation projectItem)
-      {
-         base.Edit(projectItem);
-         _editParameterListPresenter.SetVisibility(PathElementId.Simulation, isVisible: false);
       }
    }
 }
