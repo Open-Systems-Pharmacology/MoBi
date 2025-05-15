cb979eaf0b0bea77690be86457b1434ed1ddca67
benjaperez1983 <benjamin.perez@gmail.com>
2025-05-15 10:37:52 +0200

    Changes for PR

--- src/MoBi.R/Bootstrap/ApplicationStartup.cs
+++ src/MoBi.R/Bootstrap/ApplicationStartup.cs
@@ -26,28 +26,5 @@ namespace MoBi.R.Bootstrap
          container.AddRegister(x => x.FromType<InfrastructureRegister>());
          container.AddRegister(x => x.FromType<RRegister>());
       }
-
-      private static void initializeDimensions(IContainer container)
-      {
-         var applicationConfiguration = container.Resolve<IApplicationConfiguration>();
-         var dimensionFactory = container.Resolve<IDimensionFactory>();
-         var persistor = container.Resolve<IDimensionFactoryPersistor>();
-         persistor.Load(dimensionFactory, applicationConfiguration.DimensionFilePath);
-         dimensionFactory.AddDimension(Constants.Dimension.NO_DIMENSION);
-
-         var molarConcentrationDimension = dimensionFactory.Dimension(Constants.Dimension.MOLAR_CONCENTRATION);
-         var massConcentrationDimension = dimensionFactory.Dimension(Constants.Dimension.MASS_CONCENTRATION);
-         var amountDimension = dimensionFactory.Dimension(Constants.Dimension.MOLAR_AMOUNT);
-         var massDimension = dimensionFactory.Dimension(Constants.Dimension.MASS_AMOUNT);
-         var aucMolarDimension = dimensionFactory.Dimension(Constants.Dimension.MOLAR_AUC);
-         var aucMassDimension = dimensionFactory.Dimension(Constants.Dimension.MASS_AUC);
-
-         dimensionFactory.AddMergingInformation(new SimpleDimensionMergingInformation(molarConcentrationDimension, massConcentrationDimension));
-         dimensionFactory.AddMergingInformation(new SimpleDimensionMergingInformation(massConcentrationDimension, molarConcentrationDimension));
-         dimensionFactory.AddMergingInformation(new SimpleDimensionMergingInformation(amountDimension, massDimension));
-         dimensionFactory.AddMergingInformation(new SimpleDimensionMergingInformation(massDimension, amountDimension));
-         dimensionFactory.AddMergingInformation(new SimpleDimensionMergingInformation(aucMolarDimension, aucMassDimension));
-         dimensionFactory.AddMergingInformation(new SimpleDimensionMergingInformation(aucMassDimension, aucMolarDimension));
-      }
    }
 }
--- tests/MoBi.R.Tests/Services/ProjectTaskSpecs.cs
+++ tests/MoBi.R.Tests/Services/ProjectTaskSpecs.cs
@@ -3,7 +3,11 @@ using System.Linq;
 using MoBi.Core.Domain.Model;
 using MoBi.R.Services;
 using NUnit.Framework;
+using OSPSuite.BDDHelper;
 using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Data;
+using OSPSuite.R.Services;
 using static MoBi.R.Tests.DomainHelperForSpecs;
 
 namespace MoBi.R.Tests.Services
@@ -14,6 +18,34 @@ namespace MoBi.R.Tests.Services
       {
          base.GlobalContext();
          sut = Api.GetProjectTask();
+      }
+   }
+
+   internal class when_running_a_simulation_from_a_project : concern_for_ProjectTask
+   {
+      protected IModelCoreSimulation _simulation;
+      protected ISimulationRunner _simulationRunnerTask;
+      private SimulationResults _results;
+      private MoBiProject _project;
+
+      protected override void Context()
+      {
+         base.Context();
+         _simulationRunnerTask = OSPSuite.R.Api.GetSimulationRunner();
+         var projectFile = TestFileFullPath("SampleProjectWith2Simulations.mbp3");
+         _project = sut.GetProject(projectFile);
+         _simulation = _project.Simulations.FirstOrDefault();
+      }
+
+      protected override void Because()
+      {
+         _results = _simulationRunnerTask.Run(new SimulationRunArgs { Simulation = _simulation });
+      }
+
+      [Observation]
+      public void should_create_results()
+      {
+         _results.ShouldNotBeNull();
       }
    }
 
@@ -68,10 +100,8 @@ namespace MoBi.R.Tests.Services
       public void should_return_module_building_blocks_names()
       {
          _buildingBlockNames.ShouldNotBeNull();
-         _buildingBlockNames.ShouldContain("Organism");
-         _buildingBlockNames.ShouldContain("Molecules");
-         _buildingBlockNames.ShouldContain("Initial Conditions");
-         _buildingBlockNames.ShouldContain("Reactions");
+         _buildingBlockNames.ShouldOnlyContain("Organism", "Molecules", "Initial Conditions", "Reactions");
+         ;
       }
    }
 
@@ -98,8 +128,7 @@ namespace MoBi.R.Tests.Services
       {
          _simulationNames.ShouldNotBeNull();
          _simulationNames.Count.ShouldBeEqualTo(2);
-         _simulationNames.ShouldContain("Simulation1");
-         _simulationNames.ShouldContain("Simulation2");
+         _simulationNames.ShouldOnlyContain("Simulation1", "Simulation2");
       }
    }
 }
