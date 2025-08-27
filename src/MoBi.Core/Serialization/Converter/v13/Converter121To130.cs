cb9d66092da9bc79e2ca19e2c22a74b3923bc8e6
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-08-27 13:37:01 -0400

    empty project conversions

---
+++ src/MoBi.Core/Serialization/Converter/v13/Converter121To130.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Linq;
+using MoBi.Core.Domain.Model;
+using CoreConverter121To130 = OSPSuite.Core.Converters.v13.Converter121To130;
+
+namespace MoBi.Core.Serialization.Converter.v13;
+
+public class Converter121To130(CoreConverter121To130 coreConverter) : IMoBiObjectConverter
+{
+   public bool IsSatisfiedBy(int version) => version == ProjectVersions.V12_1;
+
+   public (int convertedToVersion, bool conversionHappened) Convert(object objectToUpdate, MoBiProject project)
+   {
+      (_, bool converted) = coreConverter.Convert(objectToUpdate);
+      return (ProjectVersions.V13_0, converted);
+   }
+
+   public (int convertedToVersion, bool conversionHappened) ConvertXml(XElement element, MoBiProject project)
+   {
+      (_, bool converted) = coreConverter.ConvertXml(element);
+      return (ProjectVersions.V13_0, converted);
+   }
+}
