44211f2d15f01b8fe3cc5ecf8f2f7ec2612eba4b
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-08-27 12:45:05 -0400

    Fixes #2058 (#2059)

---
+++ src/MoBi.Core/Serialization/Converter/v12/Converter120To121.cs
@@ -0,0 +1,28 @@
+﻿using MoBi.Core.Domain.Model;
+using System.Xml.Linq;
+using CoreConverter120To121 = OSPSuite.Core.Converters.v12.Converter120To121;
+
+namespace MoBi.Core.Serialization.Converter.v12
+{
+   public class Converter120To121 : IMoBiObjectConverter
+   {
+      private readonly CoreConverter120To121 _coreConverter;
+
+      public Converter120To121(CoreConverter120To121 coreConverter)
+      {
+         _coreConverter = coreConverter;
+      }
+      
+      public bool IsSatisfiedBy(int version) => version == ProjectVersions.V12_0;
+
+      public (int convertedToVersion, bool conversionHappened) Convert(object objectToUpdate, MoBiProject project)
+      {
+         return _coreConverter.Convert(objectToUpdate);
+      }
+
+      public (int convertedToVersion, bool conversionHappened) ConvertXml(XElement element, MoBiProject project)
+      {
+         return _coreConverter.ConvertXml(element);
+      }
+   }
+}
