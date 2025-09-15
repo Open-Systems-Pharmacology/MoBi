16655ae67c0367fa15e83ff15a647541e4f15857
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-09-15 10:37:22 -0400

    Fixes #2104 LoadModulesFromFile: exception when passing a pkml simulation with multiple modules.

--- src/MoBi.Presentation/Tasks/ISerializationTask.cs
+++ src/MoBi.Presentation/Tasks/ISerializationTask.cs
@@ -1,5 +1,5 @@
-using MoBi.Core.Domain.Model;
 using System.Collections.Generic;
+using MoBi.Core.Domain.Model;
 
 namespace MoBi.Presentation.Tasks
 {
@@ -9,7 +9,25 @@ namespace MoBi.Presentation.Tasks
       void SaveProject();
       void CloseProject();
       MoBiProject NewProject();
-      IEnumerable<T> LoadMany<T>(string fileName, bool resetIds = false);
+
       void LoadJournal(string journalPath, string projectFullPath = null, bool showJournal = false);
+
+      /// <summary>
+      ///    Loads multiple instances of the element type <typeparamref name="T" /> from the <paramref name="fileName" />. If
+      ///    more than one element
+      ///    of the type is present in the file, the user will confirm which elements to load by name. If
+      ///    <paramref name="resetIds" /> is true, then all element ID's will
+      ///    be set to new values on load.
+      /// </summary>
+      /// <returns>A new list of elements that were loaded</returns>
+      IReadOnlyList<T> LoadMany<T>(string fileName, bool resetIds = false);
+
+      /// <summary>
+      ///    Loads all instances of the element type <typeparamref name="T" /> from the <paramref name="fileName" />. If
+      ///    <paramref name="resetIds" /> is true, then all element ID's will
+      ///    be set to new values on load.
+      /// </summary>
+      /// <returns>A new list of elements that were loaded</returns>
+      IReadOnlyList<T> LoadAll<T>(string fileName, bool resetIds = false);
    }
 }
