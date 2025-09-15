433604a7a4b77e18200c2a0213b6cced725bbb38
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-09-15 10:40:19 -0400

    spacing

--- src/MoBi.Presentation/Tasks/ISerializationTask.cs
+++ src/MoBi.Presentation/Tasks/ISerializationTask.cs
@@ -13,19 +13,16 @@ namespace MoBi.Presentation.Tasks
       void LoadJournal(string journalPath, string projectFullPath = null, bool showJournal = false);
 
       /// <summary>
-      ///    Loads multiple instances of the element type <typeparamref name="T" /> from the <paramref name="fileName" />. If
-      ///    more than one element
-      ///    of the type is present in the file, the user will confirm which elements to load by name. If
-      ///    <paramref name="resetIds" /> is true, then all element ID's will
-      ///    be set to new values on load.
+      ///    Loads multiple instances of the element type <typeparamref name="T" /> from the <paramref name="fileName" />.
+      ///    If more than one element of the type is present in the file, the user will confirm which elements to load by name.
+      ///    If <paramref name="resetIds" /> is true, then all element ID's will be set to new values on load.
       /// </summary>
       /// <returns>A new list of elements that were loaded</returns>
       IReadOnlyList<T> LoadMany<T>(string fileName, bool resetIds = false);
 
       /// <summary>
-      ///    Loads all instances of the element type <typeparamref name="T" /> from the <paramref name="fileName" />. If
-      ///    <paramref name="resetIds" /> is true, then all element ID's will
-      ///    be set to new values on load.
+      ///    Loads all instances of the element type <typeparamref name="T" /> from the <paramref name="fileName" />.
+      ///    If <paramref name="resetIds" /> is true, then all element ID's will be set to new values on load.
       /// </summary>
       /// <returns>A new list of elements that were loaded</returns>
       IReadOnlyList<T> LoadAll<T>(string fileName, bool resetIds = false);
