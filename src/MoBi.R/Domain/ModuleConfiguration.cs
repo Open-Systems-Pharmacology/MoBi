36aa671ac43825db323b6d388b0a51600adacb7f
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-11-12 17:26:42 -0500

    separate files

--- src/MoBi.R/Domain/ModuleConfiguration.cs
+++ src/MoBi.R/Domain/ModuleConfiguration.cs
@@ -1,5 +1,4 @@
-﻿using MoBi.Core.Domain.Model;
-using OSPSuite.Core.Domain;
+﻿using OSPSuite.Core.Domain;
 using OSPSuite.Core.Domain.Builder;
 
 namespace MoBi.R.Domain;
@@ -9,13 +8,4 @@ public class ModuleConfiguration
    public Module Module { get; set; }
    public ParameterValuesBuildingBlock SelectedParameterValue { get; set; }
    public InitialConditionsBuildingBlock SelectedInitialCondition { get; set; }
-}
-
-public class MoBiSimulation : OSPSuite.R.Domain.Simulation
-{
-   public MoBiSimulation(IMoBiSimulation modelCoreSimulation) : base(modelCoreSimulation)
-   {
-   }
-   
-   public IMoBiSimulation Simulation => CoreSimulation as IMoBiSimulation;
 }
