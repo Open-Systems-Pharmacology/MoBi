f8ec7ae5066d0b887aa5b3851efd6e8143f43d91
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2026-02-18 10:58:40 -0500

    pr feedback

--- src/MoBi.Core/Services/InitialConditionPropertiesForMerge.cs
+++ src/MoBi.Core/Services/InitialConditionPropertiesForMerge.cs
@@ -7,7 +7,7 @@ public class InitialConditionPropertiesForMerge
    public ObjectPath ObjectPath { set; get; }
    public string DimensionName { set; get; }
    public double ValueInBaseUnit { set; get; }
-   public double ScaleDivisor { set; get; }
+   public double ScaleDivisor { set; get; } = Constants.DEFAULT_SCALE_DIVISOR;
    public bool IsPresent { set; get; }
-   public bool NegativeAllowed { set; get; }
+   public bool NegativeAllowed { set; get; } = false;
 }
