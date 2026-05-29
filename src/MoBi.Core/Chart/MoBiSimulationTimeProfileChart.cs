5c1b3a74b3ec0559083dc4375f7d30db97f81291
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2026-05-29 21:33:24 +0000

    update_core_13.0.118 (#2410)

--- src/MoBi.Core/Chart/MoBiSimulationTimeProfileChart.cs
+++ src/MoBi.Core/Chart/MoBiSimulationTimeProfileChart.cs
@@ -4,5 +4,6 @@ namespace MoBi.Core.Chart
 {
    public class MoBiSimulationTimeProfileChart : AnalysisChart
    {
+      public override AnalysisChartTypes AnalysisChartType => AnalysisChartTypes.TimeProfile;
    }
 }
