93b9b6ed490a67f05e1b883fff95ae5079ab1038
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2026-07-30 13:42:57 -0400

    Fixes #2466 Restore the application molecule builder editor
    This presenter/view cluster is only ever resolved through the ICreatePresenter<>
    registration convention, so it had no static references and #2412 removed it as
    dead code. Creating an application molecule builder has thrown ever since, from
    both the tree context menu and the application editor grid.

---
+++ src/MoBi.Presentation/Views/IEditApplicationMoleculeBuilderView.cs
@@ -0,0 +1,12 @@
+﻿using MoBi.Presentation.DTO;
+using MoBi.Presentation.Presenter;
+using OSPSuite.Presentation.Views;
+
+namespace MoBi.Presentation.Views
+{
+   public interface IEditApplicationMoleculeBuilderView : IView<IEditApplicationMoleculeBuilderPresenter>, IActivatableView
+   {
+      void Show(ApplicationMoleculeBuilderDTO dtoApplicationMoleculeBuilder);
+      void SetFormulaView(IView view);
+   }
+}
