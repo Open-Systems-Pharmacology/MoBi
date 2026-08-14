8f744e09a97a6ca6cee7c3d233469b586040b49b
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2026-08-14 14:04:11 -0400

    Cover undoing a change to a quantity fixed at its constant formula value

--- tests/MoBi.Tests/Core/Commands/SetQuantityValueInSimulationCommandSpecs.cs
+++ tests/MoBi.Tests/Core/Commands/SetQuantityValueInSimulationCommandSpecs.cs
@@ -123,6 +123,33 @@ namespace MoBi.Core.Commands
       }
    }
 
+   public class When_executing_the_inverse_of_setting_a_value_in_a_quantity_that_was_already_fixed_to_the_constant_formula_value : concern_for_SetQuantityValueInSimulationCommand
+   {
+      protected override IQuantity CreateQuantity()
+      {
+         var quantity = new Parameter().WithFormula(new ConstantFormula(_valueToSet));
+         quantity.Value = 7;
+         return quantity;
+      }
+
+      protected override void Because()
+      {
+         sut.ExecuteAndInvokeInverse(_context);
+      }
+
+      [Observation]
+      public void should_have_restored_the_previously_fixed_value()
+      {
+         _quantity.Value.ShouldBeEqualTo(7);
+      }
+
+      [Observation]
+      public void the_quantity_should_still_be_marked_as_fixed()
+      {
+         _quantity.IsFixedValue.ShouldBeTrue();
+      }
+   }
+
    public class When_setting_a_value_in_a_quantity_with_explicit_formula : concern_for_SetQuantityValueInSimulationCommand
    {
       protected override IQuantity CreateQuantity()
